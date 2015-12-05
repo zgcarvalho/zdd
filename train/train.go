1dff52c567ba699233d5c5d64390af412bb13f3b
Jose Geraldo <jgcarvalho@usp.br>
2015-12-05 16:21:20 -0200

    correlation added to cost function

--- train/train.go
+++ train/train.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"math/rand"
+	// "math/rand"
 	"path/filepath"
 
 	"github.com/thoj/go-galib"
@@ -13,21 +13,23 @@ import (
 	"bitbucket.org/jgcarvalho/zdd/protein"
 	"bitbucket.org/jgcarvalho/zdd/score"
 	"github.com/gonum/optimize"
+	"github.com/gonum/stat"
+
 )
 
 var params score.Parameters
 var traindata []TrainData
 var scores int
 
-func init() {
-	params = score.LoadParams("/home/jgcarvalho/gocode/src/bitbucket.org/jgcarvalho/zdd/params/INITPARAMS")
-	fn, err := filepath.Abs("./traindata/data.json")
-	if err != nil {
-		fmt.Println("Arquivo com dados de treinamento", err)
-		panic(err)
-	}
-	traindata = loadData(fn)
-}
+// func init() {
+// 	params = score.LoadParams("/home/jgcarvalho/gocode/src/bitbucket.org/jgcarvalho/zdd/params/INITPARAMS")
+// 	fn, err := filepath.Abs("./traindata/data.json")
+// 	if err != nil {
+// 		fmt.Println("Arquivo com dados de treinamento", err)
+// 		panic(err)
+// 	}
+// 	traindata = loadData(fn)
+// }
 
 type TrainData struct {
 	Name      string
@@ -53,9 +55,11 @@ func loadData(fn string) []TrainData {
 }
 
 func cost(params score.Parameters, traindata []TrainData) float64 {
-	var score float64
+	var totalScore float64
 	pkdchan := make(chan float64, len(traindata))
 	rankchan := make(chan float64, len(traindata))
+	exp := make([]float64,len(traindata))
+ 	obs := make([]float64,len(traindata))
 	pkdScore := 0.0
 	rankScore := 0.0
 	for i := 0; i < len(traindata); i++ {
@@ -72,97 +76,24 @@ func cost(params score.Parameters, traindata []TrainData) float64 {
 					rk += 0.1
 				}
 			}
+			exp[i] = traindata[i].Pkd
+			obs[i] = total
 			pkdchan <- (traindata[i].Pkd - total) * (traindata[i].Pkd - total)
 			rankchan <- rk
 		}(i)
-		// protein := protein.LoadMol2("./traindata/" + traindata[i].Receptor)
-		// pos := ligand.LoadMol2("./traindata/" + traindata[i].Positive)
-		// total := params.Score(&protein, &pos)
-		//
-		// // pkdScore += math.Abs(traindata[i].Pkd - total)
-		// pkdScore += (traindata[i].Pkd - total) * (traindata[i].Pkd - total)
-		//
-		// for j := 0; j < len(traindata[i].Negatives); j++ {
-		// 	neg := ligand.LoadMol2("./traindata/" + traindata[i].Negatives[j])
-		// 	negTotal := params.Score(&protein, &neg)
-		// 	if negTotal >= total {
-		// 		rankScore += 0.1
-		// 	}
-		// }
 	}
 
 	for i := 0; i < len(traindata); i++ {
 		pkdScore += <-pkdchan
 		rankScore += <-rankchan
 	}
+	corr := stat.Correlation(exp,obs,nil)
+	// pkdScore = pkdScore
+	// totalScore = pkdScore * rankScore
+	totalScore = pkdScore/corr + (pkdScore/corr * rankScore)
+	fmt.Printf("PKD %f - Rank %f - Corr %f- TOTAL %f\n", pkdScore, rankScore, corr, totalScore)
+	return totalScore
 
-	rankScore = pkdScore * rankScore
-	fmt.Printf("PKD %f - Rank %f - TOTAL %f\n", pkdScore, rankScore, pkdScore+rankScore)
-	score = pkdScore + rankScore
-	return score
-
-}
-
-func genNewMainParams(params score.Parameters) score.Parameters {
-	penal := rand.NormFloat64()*0.01 + params["Al_Br"].Penal
-	metalDbest := rand.NormFloat64()*0.01 + params["Al_Br"].Dbest
-	metalAlpha := rand.NormFloat64()*0.01 + params["Al_Br"].Alpha
-	metalBeta := rand.NormFloat64()*0.01 + params["Al_Br"].Beta
-	repulsiveDbest := rand.NormFloat64()*0.01 + params["Al_Ca"].Dbest
-	repulsiveAlpha := rand.NormFloat64()*0.01 + params["Al_Ca"].Alpha
-	repulsiveBeta := rand.NormFloat64()*0.01 + params["Al_Ca"].Beta
-	buriedDbest := rand.NormFloat64()*0.01 + params["Al_C.1"].Dbest
-	buriedAlpha := rand.NormFloat64()*0.01 + params["Al_C.1"].Alpha
-	buriedBeta := rand.NormFloat64()*0.01 + params["Al_C.1"].Beta
-	hbondDbest := rand.NormFloat64()*0.01 + params["N.1_O.3"].Dbest
-	hbondAlpha := rand.NormFloat64()*0.01 + params["N.1_O.3"].Alpha
-	hbondBeta := rand.NormFloat64()*0.01 + params["N.1_O.3"].Beta
-	haDbest := rand.NormFloat64()*0.01 + params["Br_O.3"].Dbest
-	haAlpha := rand.NormFloat64()*0.01 + params["Br_O.3"].Alpha
-	haBeta := rand.NormFloat64()*0.01 + params["Br_O.3"].Beta
-	harepDbest := rand.NormFloat64()*0.01 + params["Br_Br"].Dbest
-	harepAlpha := rand.NormFloat64()*0.01 + params["Br_Br"].Alpha
-	harepBeta := rand.NormFloat64()*0.01 + params["Br_Br"].Beta
-	npolarDbest := rand.NormFloat64()*0.01 + params["C.1_C.1"].Dbest
-	npolarAlpha := rand.NormFloat64()*0.01 + params["C.1_C.1"].Alpha
-	npolarBeta := rand.NormFloat64()*0.01 + params["C.1_C.1"].Beta
-	for k := range params {
-		tmp := params[k]
-		tmp.Penal = penal
-		if tmp.Type == "metal" {
-			tmp.Dbest = metalDbest
-			tmp.Alpha = metalAlpha
-			tmp.Beta = metalBeta
-		} else if tmp.Type == "repulsive" {
-			tmp.Dbest = repulsiveDbest
-			tmp.Alpha = repulsiveAlpha
-			tmp.Beta = repulsiveBeta
-		} else if tmp.Type == "buried" {
-			tmp.Dbest = buriedDbest
-			tmp.Alpha = buriedAlpha
-			tmp.Beta = buriedBeta
-		} else if tmp.Type == "hbond" {
-			tmp.Dbest = hbondDbest
-			tmp.Alpha = hbondAlpha
-			tmp.Beta = hbondBeta
-		} else if tmp.Type == "ha" {
-			tmp.Dbest = haDbest
-			tmp.Alpha = haAlpha
-			tmp.Beta = haBeta
-		} else if tmp.Type == "ha-repulsive" {
-			tmp.Dbest = harepDbest
-			tmp.Alpha = harepAlpha
-			tmp.Beta = harepBeta
-		} else if tmp.Type == "npolar" {
-			tmp.Dbest = npolarDbest
-			tmp.Alpha = npolarAlpha
-			tmp.Beta = npolarBeta
-		} else {
-			fmt.Println("Que tipo é esse?", tmp.Type)
-		}
-		params[k] = tmp
-	}
-	return params
 }
 
 func trainMain2() {
@@ -328,15 +259,20 @@ func sfcost(g *ga.GAFloatGenome) float64 {
 	return cost(params, traindata)
 }
 
-func Train(params score.Parameters) {
-	// fn, err := filepath.Abs("./traindata/data_teste.json")
-	// if err != nil {
-	// 	fmt.Println("Arquivo com dados de treinamento", err)
-	// 	panic(err)
-	// }
-	// traindata := loadData(fn)
-	// fmt.Println(traindata)
-	// trainMain(params, traindata)
-	trainMain2()
-
+func Train(method int) {
+	params = score.LoadParams("/home/jgcarvalho/gocode/src/bitbucket.org/jgcarvalho/zdd/params/INITPARAMS")
+	fn, err := filepath.Abs("./traindata/data.json")
+	if err != nil {
+		fmt.Println("Arquivo com dados de treinamento", err)
+		panic(err)
+	}
+	traindata = loadData(fn)
+	switch method {
+	case 1:
+		trainMain()
+	case 2:
+		trainMain2()
+	default:
+		fmt.Println("Method unselected")
+	}
 }
--- zdd.go
+++ zdd.go
@@ -1,20 +1,41 @@
 package main
 
 import (
-	// "flag"
-	// "fmt"
+	"flag"
+	"fmt"
 
-	// "bitbucket.org/jgcarvalho/zdd/ligand"
-	// "bitbucket.org/jgcarvalho/zdd/protein"
+	"bitbucket.org/jgcarvalho/zdd/ligand"
+	"bitbucket.org/jgcarvalho/zdd/protein"
 	"bitbucket.org/jgcarvalho/zdd/score"
 	"bitbucket.org/jgcarvalho/zdd/train"
 )
 
 func main() {
-	// f_prot := flag.String("p", "", "protein mol2")
-	// f_lig := flag.String("l", "", "ligand mol2")
-	// flag.Parse()
-	params := score.LoadParams("/home/jgcarvalho/gocode/src/bitbucket.org/jgcarvalho/zdd/params/INITPARAMS")
+	doTrain := flag.Bool("train", false, "train using genetic algorithm")
+	doTrain2 := flag.Bool("train2", false, "train using nelder mead")
+	doScore := flag.Bool("score", false, "score conformation")
+	fprot := flag.String("p", "", "protein mol2")
+	flig := flag.String("l", "", "ligand mol2")
+	flag.Parse()
+
+	if *doTrain {
+		train.Train(1)
+		return
+	}else if *doTrain2{
+		train.Train(2)
+		return
+	}else if *doScore{
+			if *fprot == "" || *flig == ""{
+				fmt.Println("Protein or ligand not set")
+				return
+			}
+			params := score.LoadParams("/home/jgcarvalho/gocode/src/bitbucket.org/jgcarvalho/zdd/params/INITPARAMS")
+			prot := protein.LoadMol2(*fprot)
+			lig := ligand.LoadMol2(*flig)
+			total := params.Score(&prot, &lig)
+			fmt.Println(total)
+	}
+	// params := score.LoadParams("/home/jgcarvalho/gocode/src/bitbucket.org/jgcarvalho/zdd/params/INITPARAMS")
 	// // fmt.Println(params)
 	// // protein := protein.LoadMol2("/home/jgcarvalho/pdbbind/core/proteinmol2/10gs_protein.mol2")
 	// protein := protein.LoadMol2(*f_prot)
@@ -24,5 +45,5 @@ func main() {
 	// // fmt.Println(ligand)
 	// total := params.Score(&protein, &ligand)
 	// fmt.Println(total)
-	train.Train(params)
+	return
 }
